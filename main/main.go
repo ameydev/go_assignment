e0ade0d6b4e023b5f8c2548d1943ba2be75d1b92
ameydev <ameyd@outlook.com>
2019-10-01 10:43:04 +0530

    Main file

---
+++ main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "github.com/ameydev/go_assignment/basic"
+
+
+func main() {
+	fmt.Println("find")
+	// listPrimeNumbers(50)
+	basic.listprimenumbers(20)
+	//	basic.listPrimeNumbers(50)
+}
